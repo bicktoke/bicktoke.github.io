9de347b37ce7bc38485dfdb38b69f9e3d85bc044
bicktoke <2951568372@qq.com>
2023-01-05 12:39:00 +0800

    Site updated: 2023-01-05 12:39:00

--- js/sun_moon.js
+++ js/sun_moon.js
@@ -1,25 +1,72 @@
 function switchNightMode() {
-  document.querySelector('body').insertAdjacentHTML('beforeend', '<div class="Cuteen_DarkSky"><div class="Cuteen_DarkPlanet"></div></div>'),
-    setTimeout(function() {
-      document.querySelector('body').classList.contains('DarkMode') ? (document.querySelector('body').classList.remove('DarkMode'), localStorage.setItem('isDark', '0'), document.getElementById('modeicon').setAttribute('xlink:href', '#icon-moon')) : (document.querySelector('body').classList.add('DarkMode'), localStorage.setItem('isDark', '1'), document.getElementById('modeicon').setAttribute('xlink:href', '#icon-sun')),
-        setTimeout(function() {
-          document.getElementsByClassName('Cuteen_DarkSky')[0].style.transition = 'opacity 3s';
-          document.getElementsByClassName('Cuteen_DarkSky')[0].style.opacity = '0';
-          setTimeout(function() {
-            document.getElementsByClassName('Cuteen_DarkSky')[0].remove();
-          }, 1e3);
-        }, 2e3)
-    })
+  document.querySelector('body').insertAdjacentHTML('beforeend', '<div class="Cuteen_DarkSky"><div class="Cuteen_DarkPlanet"><div id="sun"></div><div id="moon"></div></div></div>'),
+      setTimeout(function () {
+          document.querySelector('body').classList.contains('DarkMode') ? (document.querySelector('body').classList.remove('DarkMode'), localStorage.setItem('isDark', '0'), document.getElementById('modeicon').setAttribute('xlink:href', '#icon-moon')) : (document.querySelector('body').classList.add('DarkMode'), localStorage.setItem('isDark', '1'), document.getElementById('modeicon').setAttribute('xlink:href', '#icon-sun')),
+              setTimeout(function () {
+                  document.getElementsByClassName('Cuteen_DarkSky')[0].style.transition = 'opacity 3s';
+                  document.getElementsByClassName('Cuteen_DarkSky')[0].style.opacity = '0';
+                  setTimeout(function () {
+                      document.getElementsByClassName('Cuteen_DarkSky')[0].remove();
+                  }, 1e3);
+              }, 2e3)
+      })
   const nowMode = document.documentElement.getAttribute('data-theme') === 'dark' ? 'dark' : 'light'
   if (nowMode === 'light') {
-    activateDarkMode()
-    saveToLocal.set('theme', 'dark', 2)
-    GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night)
-    document.getElementById('modeicon').setAttribute('xlink:href', '#icon-sun')
+      // 先设置太阳月亮透明度
+      document.getElementById("sun").style.opacity = "1";
+      document.getElementById("moon").style.opacity = "0";
+      setTimeout(function () {
+          document.getElementById("sun").style.opacity = "0";
+          document.getElementById("moon").style.opacity = "1";
+      }, 1000);
+
+      activateDarkMode()
+      saveToLocal.set('theme', 'dark', 2)
+      // GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night)
+      document.getElementById('modeicon').setAttribute('xlink:href', '#icon-sun')
+      // 延时弹窗提醒
+      setTimeout(() => {
+          new Vue({
+              data: function () {
+                  this.$notify({
+                      title: "关灯啦🌙",
+                      message: "当前已成功切换至夜间模式！",
+                      position: 'top-left',
+                      offset: 50,
+                      showClose: true,
+                      type: "success",
+                      duration: 5000
+                  });
+              }
+          })
+      }, 2000)
   } else {
-    activateLightMode()
-    saveToLocal.set('theme', 'light', 2)
-    document.querySelector('body').classList.add('DarkMode'), document.getElementById('modeicon').setAttribute('xlink:href', '#icon-moon')
+      // 先设置太阳月亮透明度
+      document.getElementById("sun").style.opacity = "0";
+      document.getElementById("moon").style.opacity = "1";
+      setTimeout(function () {
+          document.getElementById("sun").style.opacity = "1";
+          document.getElementById("moon").style.opacity = "0";
+      }, 1000);
+      
+      activateLightMode()
+      saveToLocal.set('theme', 'light', 2)
+      document.querySelector('body').classList.add('DarkMode'), document.getElementById('modeicon').setAttribute('xlink:href', '#icon-moon')
+      setTimeout(() => {
+          new Vue({
+              data: function () {
+                  this.$notify({
+                      title: "开灯啦🌞",
+                      message: "当前已成功切换至白天模式！",
+                      position: 'top-left',
+                      offset: 50,
+                      showClose: true,
+                      type: "success",
+                      duration: 5000
+                  });
+              }
+          })
+      }, 2000)
   }
   // handle some cases
   typeof utterancesTheme === 'function' && utterancesTheme()
